b83b9e92f85437e4349a8ad578766c123e93e9c6
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-06-06 21:36:49 +0700

    Update

---
+++ build_and_flash.sh
@@ -0,0 +1,12 @@
+git clone https://github.com/NguyenHoangTrieu/esp_rust.git
+cp esp_rust/Cargo.toml Cargo.toml
+cp sdkconfig.defaults sdkconfig.defaults
+cp esp_rust/src/main1/main.rs src/main.rs
+cargo build --release --target xtensa-esp32-espidf
+espflash flash target/xtensa-esp32-espidf/release/hello_world --chip esp32 --baud 460800 --port /dev/ttyUSB0
+cp esp_rust/src/main2/main.rs src/main.rs
+cargo build --release --target xtensa-esp32-espidf
+espflash flash target/xtensa-esp32-espidf/release/hello_world --chip esp32 --baud 460800 --port /dev/ttyUSB0
+cp esp_rust/src/main3/main.rs src/main.rs
+cargo build --release --target xtensa-esp32-espidf
+espflash flash target/xtensa-esp32-espidf/release/hello_world --chip esp32 --baud 460800 --port /dev/ttyUSB0
