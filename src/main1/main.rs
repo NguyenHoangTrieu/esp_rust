9db4d8089dae3d5bbcebc1444f83c5ff1d534aa2
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-06-08 12:12:46 +0700

    Update main.rs

--- src/main1/main.rs
+++ src/main1/main.rs
@@ -3,7 +3,7 @@ use esp_idf_svc::hal::task::block_on;
 use log::*;
 
 // Define the service UUID the central is looking for
-const SERVICE_UUID: BleUuid = BleUuid::Uuid16(0xFD0D);
+const SERVICE_UUID: BleUuid = BleUuid::Uuid16(0xABCD);
 
 fn main() -> anyhow::Result<()> {
   esp_idf_svc::sys::link_patches();
