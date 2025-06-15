201c55fd0aacb3d84eb1819ad477cc58b29640ac
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-06-15 12:43:34 +0700

    Update

--- simulator/E32_Module.rs
+++ simulator/E32_Module.rs
@@ -1,5 +1,5 @@
 #[repr(usize)] // bảo đảm giá trị enum = số nguyên
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 enum ParamsOrder {
     Head = 0,
     Addh = 1,
@@ -9,10 +9,111 @@ enum ParamsOrder {
     Option = 5,
 }
 
-const CONF_SIZE: usize = 6;
+#[repr(u8)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum UartBps {
+    Bps1200 = 0b000,
+    Bps2400 = 0b001,
+    Bps4800 = 0b010,
+    Bps9600 = 0b011,
+    Bps19200 = 0b100,
+    Bps38400 = 0b101,
+    Bps57600 = 0b110,
+    Bps115200 = 0b111,
+}
+
+// #[repr(u8)]
+// #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+// pub enum As32UartParity {
+//     Mode8N1 = 0b00,
+//     Mode8O1 = 0b01,
+//     Mode8E1 = 0b10,
+//     Mode8N1_2 = 0b11, // đặt tên khác vì trùng với 0b00
+// }
+
+#[repr(u8)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum AirDataRate {
+    Rate300 = 0b000,
+    Rate1200 = 0b001,
+    Rate2400 = 0b010,
+    Rate4800 = 0b011,
+    Rate9600 = 0b100,
+    Rate19200 = 0b101,
+}
+
+// #[repr(u8)]
+// #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+// pub enum TransmissionPower {
+//     Power20 = 0b00,
+//     Power17 = 0b01,
+//     Power14 = 0b10,
+//     Power10 = 0b11,
+// }
+
+// #[repr(u8)]
+// #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+// pub enum WirelessWakeUpTime {
+//     WakeUp250 = 0b000,
+//     WakeUp500 = 0b001,
+//     WakeUp750 = 0b010,
+//     WakeUp1000 = 0b011,
+//     WakeUp1250 = 0b100,
+//     WakeUp1500 = 0b101,
+//     WakeUp1750 = 0b110,
+//     WakeUp2000 = 0b111,
+// }
+
+// #[repr(u8)]
+// #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+// pub enum FixedTransmission {
+//     Transparent = 0,
+//     PointToPoint = 1,
+// }
+
+// #[repr(u8)]
+// #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+// pub enum IoDriveMode {
+//     OpenCollector = 0b0,
+//     PushPull = 0b1,
+// }
+
+// #[repr(u8)]
+// #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+// pub enum NormalCommand {
+//     ReadConfig = 0,
+//     ReadVersion = 1,
+//     ResetModule = 2,
+//     ReadVoltage = 3,
+//     RestoreDefaults = 4,
+//     Handshake = 5,
+//     ReadSoftwareVersion = 6,
+//     ReadRssiSignal = 7,
+//     ReadRssiEnvironment = 8,
+// }
+
+// #[repr(u8)]
+// #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+// pub enum ConfigCommand {
+//     WriteSaved = 0,
+//     WriteTemporary = 1,
+// }
+
+#[repr(u8)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum E32State {
+    Normal,
+    WakeUp,
+    PowerSaving,
+    Sleep,
+}
+
+pub const CONF_SIZE: usize = 6;
 
 pub struct E32Module {
     params_list: [u8; CONF_SIZE],
+    pub air_data_rate: AirDataRate,
+    pub uart_bps: UartBps,
 }
 
 impl E32Module {
@@ -24,10 +125,55 @@ impl E32Module {
         params_list[ParamsOrder::Sped as usize]   = 0x1A;
         params_list[ParamsOrder::Chan as usize]   = 0x17;
         params_list[ParamsOrder::Option as usize] = 0x44;
+        let sped = params_list[ParamsOrder::Sped as usize];
 
-        Self { params_list }
+        let air_data_rate = match sped & 0b0000_0111 {
+            0b000 => AirDataRate::Rate300,
+            0b001 => AirDataRate::Rate1200,
+            0b010 => AirDataRate::Rate2400,
+            0b011 => AirDataRate::Rate4800,
+            0b100 => AirDataRate::Rate9600,
+            0b101 | 0b110 | 0b111 => AirDataRate::Rate19200,
+            _ => AirDataRate::Rate2400, // default fallback
+        };
+
+        let uart_bps = match (sped >> 3) & 0b0000_0111 {
+            0b000 => UartBps::Bps1200,
+            0b001 => UartBps::Bps2400,
+            0b010 => UartBps::Bps4800,
+            0b011 => UartBps::Bps9600,
+            0b100 => UartBps::Bps19200,
+            0b101 => UartBps::Bps38400,
+            0b110 => UartBps::Bps57600,
+            0b111 => UartBps::Bps115200,
+            _ => UartBps::Bps9600, // fallback
+        };
+
+        Self {
+            params_list,
+            air_data_rate,
+            uart_bps,
+        }
     }
+    pub fn input_command(&mut self, command: &[u8], size: usize) -> String {
+        const CONF_SIZE: usize = 6;
+        let mut buffer = [0u8; CONF_SIZE];
 
+        if command.len() == CONF_SIZE && command[0] == 0xC0 {
+            self.set_params(command, size);
+            "OK".to_string()
+        } 
+        else if command.len() == 3 && command[0] == 0xC1 && command[1] == 0xC1 && command[2] == 0xC1 {
+            self.get_params(&mut buffer);
+            buffer.iter()
+                .map(|b| format!("{:02X}", b))
+                .collect::<Vec<_>>()
+                .join(" ")
+        } 
+        else {
+            "ERROR".to_string()
+        }
+    }
     pub fn set_params(&mut self, params: &[u8], size: usize) {
         if size != CONF_SIZE {
             return;
@@ -35,6 +181,29 @@ impl E32Module {
         for i in 0..size {
             self.params_list[i] = params[i];
         }
+        let sped = self.params_list[ParamsOrder::Sped as usize];
+
+        self.air_data_rate = match sped & 0b0000_0111 {
+            0b000 => AirDataRate::Rate300,
+            0b001 => AirDataRate::Rate1200,
+            0b010 => AirDataRate::Rate2400,
+            0b011 => AirDataRate::Rate4800,
+            0b100 => AirDataRate::Rate9600,
+            0b101 | 0b110 | 0b111 => AirDataRate::Rate19200,
+            _ => AirDataRate::Rate2400, // default fallback
+        };
+
+        self.uart_bps = match (sped >> 3) & 0b0000_0111 {
+            0b000 => UartBps::Bps1200,
+            0b001 => UartBps::Bps2400,
+            0b010 => UartBps::Bps4800,
+            0b011 => UartBps::Bps9600,
+            0b100 => UartBps::Bps19200,
+            0b101 => UartBps::Bps38400,
+            0b110 => UartBps::Bps57600,
+            0b111 => UartBps::Bps115200,
+            _ => UartBps::Bps9600, // fallback
+        };
     }
 
     pub fn get_params(&self, buffer: &mut [u8]) {
--- simulator/main.rs
+++ simulator/main.rs
@@ -1,27 +1,22 @@
 use esp_idf_hal::gpio::*;
 use esp_idf_hal::prelude::*;
-use esp_idf_hal::uart::*;
 use esp_idf_hal::delay::*;
-use embedded_hal::serial::{Read, Write};
-
-mod simulator::e32_module;
-mod simulator::buffer;
-
+use esp_idf_hal::*;
+use core::sync::atomic::{AtomicBool, Ordering};
+mod simulator{
+    pub mod e32_module;
+    pub mod buffer;
+}
 use simulator::e32_module::*;
 use simulator::buffer::*;
 
 const BUFF_SIZE: usize = 256;
 const MAX_WAIT_TIMES: u8 = 3;
-const BYTE_TIME_115200: u64 = 70;  // us
-const BYTE_TIME_9600: u64 = 833;   // us
+const BYTE_TIME_57600: u64 = 70*2;  // us
+const BYTE_TIME_19200: u64 = 416;   // us
+static TIMER0_EXPIRED: AtomicBool = AtomicBool::new(false);
+static TIMER1_EXPIRED: AtomicBool = AtomicBool::new(false);
 
-#[derive(PartialEq, Copy, Clone)]
-enum E32State {
-    Normal,
-    WakeUp,
-    PowerSaving,
-    Sleep,
-}
 
 fn main() -> anyhow::Result<()> {
     esp_idf_sys::link_patches();
@@ -29,29 +24,49 @@ fn main() -> anyhow::Result<()> {
     // Init peripherals
     let peripherals = Peripherals::take().unwrap();
     let pins = peripherals.pins;
-
-    // UART0: STM32 ↔ ESP32
-    let mut uart0 = UartDriver::new(
+    // init timer for UART
+    let timer_conf = timer::config::Config::new().auto_reload(false);
+    let mut timer0 = timer::TimerDriver::new(peripherals.timer00, &timer_conf)?;
+    timer0.set_alarm(timer0.tick_hz() / 1000000 * BYTE_TIME_57600)?;
+    unsafe {
+        timer0.subscribe(move || {
+            TIMER0_EXPIRED.store(true, Ordering::Relaxed);
+        })?;
+    }
+    timer0.enable_interrupt()?;
+    timer0.enable_alarm(false)?;
+    timer0.enable(false)?;
+    // UART0: PC ↔ ESP32
+    let uart0 = uart::UartDriver::new(
         peripherals.uart0,
         pins.gpio1,  // TX0
         pins.gpio3,  // RX0
         Option::<AnyIOPin>::None,
         Option::<AnyIOPin>::None,
-        &Config::default().baudrate(Hertz(115_200)),
+        &uart::config::Config::default().baudrate(Hertz(115_200)),
     )?;
-
-    // UART1: ESP32 ↔ Laptop
-    let mut uart1 = UartDriver::new(
+    let mut timer1 = timer::TimerDriver::new(peripherals.timer01, &timer_conf)?;
+    timer1.set_alarm(timer1.tick_hz() / 1000000 * BYTE_TIME_19200)?;
+    unsafe {
+        timer1.subscribe(move || {
+            TIMER1_EXPIRED.store(true, Ordering::Relaxed);
+        })?;
+    }
+    timer1.enable_interrupt()?;
+    timer1.enable_alarm(false)?;
+    timer1.enable(false)?;
+    // UART1: ESP32 ↔ STM32
+    let uart1 = uart::UartDriver::new(
         peripherals.uart1,
         pins.gpio12, // TX1
         pins.gpio13, // RX1
         Option::<AnyIOPin>::None,   
         Option::<AnyIOPin>::None,
-        &Config::default().baudrate(Hertz(9_600)),
+        &uart::config::Config::default().baudrate(Hertz(9_600)),
     )?;
 
-    let mut m0 = PinDriver::input(pins.gpio4)?; // M0
-    let mut m1 = PinDriver::input(pins.gpio16)?; // M1
+    let m0 = PinDriver::input(pins.gpio4)?; // M0
+    let m1 = PinDriver::input(pins.gpio16)?; // M1
     let mut aux = PinDriver::output(pins.gpio2)?; // AUX
     aux.set_high()?; // AUX HIGH ban đầu
 
@@ -59,66 +74,56 @@ fn main() -> anyhow::Result<()> {
     let mut upper_buffer = Buffer::new(BUFF_SIZE);
     let mut e32 = E32Module::new();
 
-    let mut state = E32State::Sleep;
-    let mut delay = Delay::new_default();
-
-    let mut current_baudrate = 115_200;
-
-    let mut low_buff_time = delay.get_systimer_count();
-    let mut up_buff_time = delay.get_systimer_count();
+    let mut state;
     let mut low_wait = 0;
     let mut up_wait = 0;
     let mut low_last_size = 0;
     let mut up_last_size = 0;
-
+    let mut buf = [0_u8; 1];
+    let mut buf1 = [0_u8; 1];
+    let mut params_buf = [0_u8; CONF_SIZE];
     loop {
         // Check state change
         let new_state = match (m0.is_high(), m1.is_high()) {
-            (Ok(false), Ok(false)) => E32State::Normal,
-            (Ok(true), Ok(false)) => E32State::WakeUp,
-            (Ok(false), Ok(true)) => E32State::PowerSaving,
-            (Ok(true), Ok(true)) => E32State::Sleep,
-            _ => state,
+            (false, false) => E32State::Normal,
+            (true, false) => E32State::WakeUp,
+            (false, true) => E32State::PowerSaving,
+            (true, true) => E32State::Sleep,
         };
-
-        if new_state != state {
-            state = new_state;
-            if state == E32State::Sleep {
-                uart0.set_baudrate(Hertz(CONF_BAUD as u32))?;
-                delay.delay_ms(2u32);
-            } else {
-                uart0.set_baudrate(Hertz(115_200))?;
-                delay.delay_ms(1u32);
-            }
-        }
-
+        state = new_state;
         match state {
             E32State::Normal => {
-                // Read from UART0 (STM32) → lower buffer
-                while let Ok(b) = uart0.read() {
-                    lower_buffer.enqueue(b);
+                // Read from UART0 (PC) → upper buffer
+                while let Ok(_b) = uart0.read(&mut buf, BLOCK) {
+                    upper_buffer.enqueue(buf[0]);
                 }
 
-                // Read from UART1 (Laptop) → upper buffer
-                while let Ok(b) = uart1.read() {
-                    upper_buffer.enqueue(b);
+                // Read from UART1 (STM32) → lower buffer
+                while let Ok(_b) = uart0.read(&mut buf1, BLOCK) {
+                    lower_buffer.enqueue(buf1[0]);
                 }
 
                 // Handle lower_buffer → UART1
                 if lower_buffer.available() > 0 {
-                    let now = delay.get_systimer_count();
                     let current_size = lower_buffer.available();
                     if current_size != low_last_size {
-                        low_buff_time = now;
+                        TIMER0_EXPIRED.store(false, Ordering::Relaxed);
+                        timer0.enable_alarm(true)?;
+                        timer0.enable(true)?;
                         low_wait = 0;
                         low_last_size = current_size;
-                    } else if now - low_buff_time >= BYTE_TIME_115200 {
-                        low_buff_time = now;
+                    } else if TIMER0_EXPIRED.load(Ordering::Relaxed) {
+                        TIMER0_EXPIRED.store(false, Ordering::Relaxed);
+                        timer0.enable(true)?;
+                        timer0.enable_alarm(true)?;
                         low_wait += 1;
                         if low_wait >= MAX_WAIT_TIMES {
+                            TIMER0_EXPIRED.store(false, Ordering::Relaxed);
+                            timer0.enable(false)?;
+                            timer0.enable_alarm(false)?;
                             aux.set_low()?;
                             let data = lower_buffer.deallqueue();
-                            uart1.write_bytes(&data)?;
+                            uart1.write(&data)?;
                             aux.set_high()?;
                         }
                     }
@@ -126,36 +131,57 @@ fn main() -> anyhow::Result<()> {
 
                 // Handle upper_buffer → UART0
                 if upper_buffer.available() > 0 {
-                    let now = delay.get_systimer_count();
                     let current_size = upper_buffer.available();
                     if current_size != up_last_size {
-                        up_buff_time = now;
+                        TIMER1_EXPIRED.store(false, Ordering::Relaxed);
+                        timer0.enable(true)?;
+                        timer0.enable_alarm(true)?;
                         up_wait = 0;
                         up_last_size = current_size;
-                    } else if now - up_buff_time >= BYTE_TIME_9600 {
-                        up_buff_time = now;
+                    } else if TIMER1_EXPIRED.load(Ordering::Relaxed){
+                        TIMER1_EXPIRED.store(false, Ordering::Relaxed);
+                        timer0.enable(true)?;
+                        timer0.enable_alarm(true)?;
                         up_wait += 1;
                         if up_wait >= MAX_WAIT_TIMES {
+                            TIMER1_EXPIRED.store(false, Ordering::Relaxed);
+                            timer0.enable(false)?;
+                            timer0.enable_alarm(false)?;
+                            aux.set_low()?;
                             aux.set_low()?;
                             let data = upper_buffer.deallqueue();
-                            for b in data {
-                                uart0.write(&b)?;
-                            }
+                            uart0.write(&data)?;
                             aux.set_high()?;
                         }
                     }
                 }
             }
             E32State::Sleep => {
-                if let Ok(b) = uart0.read() {
+                if let Ok(_b) = uart1.read(&mut params_buf, BLOCK) {
                     aux.set_low()?;
-                    let mut buffer = [0u8; CONF_SIZE];
-                    buffer[0] = b;
-                    for i in 1..CONF_SIZE {
-                        buffer[i] = uart0.read().unwrap_or(0);
-                    }
-                    e32.set_params(&buffer, CONF_SIZE);
-                    uart0.write_bytes(&buffer)?;
+                    let result = e32.input_command(&params_buf.as_ref(), params_buf.len());
+                    uart1.write(result.as_bytes())?;
+                    let new_baud_rate = match e32.uart_bps {
+                        UartBps::Bps1200 => Hertz(1200),
+                        UartBps::Bps2400 => Hertz(2400),
+                        UartBps::Bps4800 => Hertz(4800),
+                        UartBps::Bps9600 => Hertz(9600),
+                        UartBps::Bps19200 => Hertz(19200),
+                        UartBps::Bps38400 => Hertz(38400),
+                        UartBps::Bps57600 => Hertz(57600),
+                        UartBps::Bps115200 => Hertz(115200),
+
+                    };
+                    let new_air_data_rate = match e32.air_data_rate {
+                        AirDataRate::Rate300 => Hertz(300),
+                        AirDataRate::Rate1200 => Hertz(1200),
+                        AirDataRate::Rate2400 => Hertz(2400),
+                        AirDataRate::Rate4800 => Hertz(4800),
+                        AirDataRate::Rate9600 => Hertz(9600),
+                        AirDataRate::Rate19200 => Hertz(19200),
+                    };
+                    uart0.change_baudrate(new_air_data_rate)?;
+                    uart1.change_baudrate(new_baud_rate)?;
                     aux.set_high()?;
                 }
             }
