6174832ac52675729a3de7da2ab862c1584e0934
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-06-19 22:35:28 +0700

    Update build_and_flash.sh

--- build_and_flash.sh
+++ build_and_flash.sh
@@ -7,3 +7,4 @@ espflash flash target/xtensa-esp32-espidf/release/hello_world --chip esp32 --bau
 cp esp_rust/src/main3/main.rs src/main.rs
 cargo build --release --target xtensa-esp32-espidf
 espflash flash target/xtensa-esp32-espidf/release/hello_world --chip esp32 --baud 460800 --port /dev/ttyUSB2
+espflash monitor --chip esp32 --port /dev/ttyUSB2 --baud 19200
