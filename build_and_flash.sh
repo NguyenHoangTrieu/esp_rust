091800e183604db56e5c83fd3b9c0d0c0e95e769
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-06-28 15:07:10 +0700

    Update build_and_flash.sh

--- build_and_flash.sh
+++ build_and_flash.sh
@@ -1,5 +1,5 @@
 cp esp_rust/simulator/main.rs src/main.rs
-cp esp_rust/simulator/Buffer.rs src/simulator/buffer.rs
+cp esp_rust/simulator/buffer.rs src/simulator/buffer.rs
 cargo build --release --target xtensa-esp32-espidf
 espflash flash target/xtensa-esp32-espidf/release/hello_world --chip esp32 --baud 460800 --port /dev/ttyUSB0
 #cp esp_rust/src/main2/main.rs src/main.rs
