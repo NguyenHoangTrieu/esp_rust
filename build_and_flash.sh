936c60254df8465907f675ddeaad820cd100f38f
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-06-28 09:49:03 +0700

    Update

--- build_and_flash.sh
+++ build_and_flash.sh
@@ -2,10 +2,10 @@ cp esp_rust/src/main1/main.rs src/main.rs
 cp esp_rust/simulator/Buffer.rs src/simulator/buffer.rs
 cargo build --release --target xtensa-esp32-espidf
 espflash flash target/xtensa-esp32-espidf/release/hello_world --chip esp32 --baud 460800 --port /dev/ttyUSB0
-cp esp_rust/src/main2/main.rs src/main.rs
-cargo build --release --target xtensa-esp32-espidf
+#cp esp_rust/src/main2/main.rs src/main.rs
+#cargo build --release --target xtensa-esp32-espidf
 espflash flash target/xtensa-esp32-espidf/release/hello_world --chip esp32 --baud 460800 --port /dev/ttyUSB1
-cp esp_rust/src/main3/main.rs src/main.rs
-cargo build --release --target xtensa-esp32-espidf
+#cp esp_rust/src/main3/main.rs src/main.rs
+#cargo build --release --target xtensa-esp32-espidf
 espflash flash target/xtensa-esp32-espidf/release/hello_world --chip esp32 --baud 460800 --port /dev/ttyUSB2
-espflash monitor --chip esp32 --port /dev/ttyUSB2 --baud 19200
+#espflash monitor --chip esp32 --port /dev/ttyUSB2 --baud 19200
